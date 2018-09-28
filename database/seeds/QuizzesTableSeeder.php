22d249ea58412e4cb70cc4019e23c8186dd20896
YehTingWei <yeh@ponos.co.jp>
2018-09-28 11:46:43 +0900

    update quize table seeder

--- database/seeds/QuizzesTableSeeder.php
+++ database/seeds/QuizzesTableSeeder.php
@@ -16,20 +16,23 @@ class QuizzesTableSeeder extends Seeder
         	'option1' => '1',
         	'option2' => '2',
         	'option3' => '3',
+            'answer' => '2',
         ]);
 
         DB::table('quizzes')->insert([
         	'question' => '2+2=?',
         	'option1' => '2',
         	'option2' => '4',
-        	'option3' => '6',
+        	'option3' => '2',
+            'answer' => '2',
         ]);
 
         DB::table('quizzes')->insert([
         	'question' => '3+1=?',
         	'option1' => '5',
         	'option2' => '2',
-        	'option3' => '6',
+        	'option3' => '4',
+            'answer' => '3',
         ]);
     }
 }
